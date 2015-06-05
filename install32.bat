43fbec90713f155577acf10eef64e9bd3fbe9bf9
Tomer Zait <tomerz@ironsrc.com>
2015-06-05 15:58:08 +0300

    Install scripts fixed

--- install32.bat
+++ install32.bat
@@ -2,5 +2,4 @@
 set PLUGINDIR=..\x64_dbg\bin\x32\plugins
 mkdir %PLUGINDIR%
 copy bin\x32\x64dbg-python.dll %PLUGINDIR%\x64dbg_python.dp32
-mkdir %PLUGINDIR%\x64dbg_python
-copy swig\build\lib.win32-2.7\* %PLUGINDIR%\x64dbg_python
+xcopy swig\build\lib.win32-2.7\x64dbg_python %PLUGINDIR%\x64dbg_python\
--- install64.bat
+++ install64.bat
@@ -2,5 +2,4 @@
 set PLUGINDIR=..\x64_dbg\bin\x64\plugins
 mkdir %PLUGINDIR%
 copy bin\x64\x64dbg-python.dll %PLUGINDIR%\x64dbg_python.dp64
-mkdir %PLUGINDIR%\x64dbg_python
-copy swig\build\lib.win-amd64-2.7\* %PLUGINDIR%\x64dbg_python
+xcopy swig\build\lib.win-amd64-2.7\x64dbg_python %PLUGINDIR%\x64dbg_python\
